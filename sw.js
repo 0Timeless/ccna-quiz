7c41c67c2bc202d683bc9f58aab19fe419051842
0Timeless <yashiromarukun21@gmail.com>
2026-05-06 19:04:34 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,37 @@
-const CACHE_NAME = 'ccna-quiz-v5';
+const CACHE_NAME = 'ccna-quiz-v6';
 const ASSETS = [
   './',
   './index.html',
   './questions.js',
-  './manifest.json',
-  './icon-512.png'
+  './manifest.json'
 ];
 
+// Sofort aktivieren, alten SW nicht abwarten
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
+// Alten Cache sofort löschen
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Network-first: immer zuerst vom Server laden, nur bei Fehler aus Cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
